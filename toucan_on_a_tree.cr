864a67a14b99cf8803efb04f79835ca5d6205a5f
Lampros Chaidas <lampros@chaidas.com>
2019-03-09 23:51:37 -0800

    check definitions from TOML file are now added as scheduled checks based on their interval

--- toucan_on_a_tree.cr
+++ toucan_on_a_tree.cr
@@ -1,49 +1,80 @@
+require "toml"   # reads our TOML configuration files - see https://github.com/crystal-community/toml.cr
 require "tasker" # this helps us schedule stuff - see https://github.com/spider-gazelle/tasker
+require "./startup_checks.cr"
+
 
 # settings some variables to make things look pretty
 OK   = "[  OK  ]"
 INFO = "[ INFO ]"
 FAIL = "[ FAIL ]"
 SETTINGS_FILE = "./settings.toml"
+CHECKS_FILE   = "./checks.toml"
+status = Hash(String, Int32).new(0)
 
 ##########################################################################################
 # Include functions from files
 ##########################################################################################
 
 # include functions from the ./startup_checks.cr file
-#include StartupChecks
-
-# include functions from the ./docker_logs_startup_checks.cr file
-#include DockerLogsStartupChecks
-
-# include functions from the ./functions_docker.cr file
-#include FunctionsDocker
-
-# include functions from the ./functions_database.cr file
-#include FunctionsDatabase
+include StartupChecks
 
 ##########################################################################################
 # Startup checks
 ##########################################################################################
 
-def check_executor(command_string)
-	output=`#{command_string}`
-#	puts `#{command_string}`
-#	puts "Exit code: #{$?.exit_status}"
-	#puts output
-	return $?.exit_status
+# the check_if_settings_file_exists function is in ./startup_checks.cr
+check_if_settings_file_exists(CHECKS_FILE)
+
+puts "#{INFO} - Parsing \"#{CHECKS_FILE}\" - \"checks\" file..."
+CHECKS = TOML.parse_file(CHECKS_FILE).as(Hash)
+
+# use this function to increase the count for a check
+# (add the check to the hash if not already there)
+def increment_failures_by_one(status_hash,unique_name)
+	status_hash[unique_name]=status_hash[unique_name]+1
+end
+
+# use this function to reset the count for a check
+def reset(status_hash,unique_name)
+	status_hash[unique_name]=0
+end
+
+def check_executor(status_hash,unique_name,command_string)
+	puts "#{Time.now} - Running check \"#{command_string}\"..."
+	output      = `#{command_string}`
+	exit_status = $?.exit_status
+	if exit_status == 0
+		reset(status_hash,unique_name)
+	else
+		increment_failures_by_one(status_hash,unique_name)
+	end
+	puts "#{Time.now} - Check \"#{command_string}\" result: #{exit_status} - current count: #{status_hash[unique_name]}"
+
 end
 
 # create a scheduler
 schedule = Tasker.instance
-schedule.every(5.seconds) { check_executor("/usr/lib/nagios/plugins/check_http -H www.google.com -t10 -c5") }
-#task = schedule.every(5.seconds) { check_executor("/usr/lib/nagios/plugins/check_http -H www.bing.com -t10 -c5") }
 
-#puts task.get
+# go through the TOML hash and add checks to the scheduler
+CHECKS.each do |host,hash_of_checks|
+	puts "#{INFO} - Importing checks for \"#{host}\"..."
+	checks=hash_of_checks.as(Hash)
+	checks.each do |check,parameters|
+		unique_name      = "#{host}_#{check}"
+		description      = parameters.as(Hash)["description"]
+		command_string   = parameters.as(Hash)["command_string"]
+		interval_seconds = parameters.as(Hash)["interval_seconds"]
+
+		# apply the schedule from the TOML file
+		schedule.every(("#{interval_seconds}".to_i).seconds) { check_executor(status,unique_name,command_string) }
+		# also if it's the first run, run them right now so we're all caught up yo
+            	schedule.in(1.seconds) { check_executor(status,unique_name,command_string) } 
+	end
+end
+
 
 
 # loop forever! (but pause every second so we don't kill the cpu)
 while true
-	puts "one sec has passed"
 	sleep 1
 end
